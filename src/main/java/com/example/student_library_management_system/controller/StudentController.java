3a2d08b3a66098ed316598726e603f52b98ee811
sunil-reddy-2003 <sunilreddymandadi@gmail.com>
2025-10-15 09:37:31 +0530

    created student controller,service and card controller and services classes

--- src/main/java/com/example/student_library_management_system/controller/StudentController.java
+++ src/main/java/com/example/student_library_management_system/controller/StudentController.java
@@ -1,4 +1,23 @@
 package com.example.student_library_management_system.controller;
 
+import com.example.student_library_management_system.requestdto.StudentRequestDto;
+import com.example.student_library_management_system.service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/student/apis")
 public class StudentController {
+
+    @Autowired
+    StudentService studentService;
+
+
+    @PostMapping("/save")
+    public String saveStudent(@RequestBody StudentRequestDto studentRequestDto){
+        return studentService.addStudent(studentRequestDto);
+    }
 }
--- src/main/java/com/example/student_library_management_system/service/StudentService.java
+++ src/main/java/com/example/student_library_management_system/service/StudentService.java
@@ -1,4 +1,33 @@
 package com.example.student_library_management_system.service;
 
+import com.example.student_library_management_system.converters.StudentConverter;
+import com.example.student_library_management_system.enums.CardStatus;
+import com.example.student_library_management_system.model.Card;
+import com.example.student_library_management_system.model.Student;
+import com.example.student_library_management_system.repository.StudentRepository;
+import com.example.student_library_management_system.requestdto.StudentRequestDto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+
+@Service
 public class StudentService {
+
+    @Autowired
+    StudentRepository studentRepository;
+
+    public String addStudent(StudentRequestDto studentRequestDto){
+        Student student=StudentConverter.convertStudentRequestInToStudentModel(studentRequestDto);
+
+        //the card will be created as the student is created
+
+        Card card=new Card();
+        card.setCardStatus(CardStatus.ACTIVE);
+        card.setExpiryDate(LocalDate.now().plusYears(4).toString());
+        card.setStudent(student);
+        student.setCard(card);
+        studentRepository.save(student);
+        return "Student saved successfully";
+    }
 }
