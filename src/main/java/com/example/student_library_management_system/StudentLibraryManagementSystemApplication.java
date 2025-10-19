98132768b2af29dcf226db07af13cb54accf9731
sunil-reddy-2003 <sunilreddymandadi@gmail.com>
2025-10-19 15:40:54 +0530

    annotations

--- src/main/java/com/example/student_library_management_system/StudentLibraryManagementSystemApplication.java
+++ src/main/java/com/example/student_library_management_system/StudentLibraryManagementSystemApplication.java
@@ -1,9 +1,13 @@
 package com.example.student_library_management_system;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
+//@Configuration+ @EnableAutoConfiguration + @ComponentScan
 public class StudentLibraryManagementSystemApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/example/student_library_management_system/controller/StudentController.java
+++ src/main/java/com/example/student_library_management_system/controller/StudentController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+//@Controller+@ResponseBody=@RestController
 @RequestMapping("/student/apis")
 public class StudentController {
 
